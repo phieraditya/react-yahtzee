6e445239e5afa73a075e80510bfcc46e1405befd
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-06-20 11:02:06 +0700

    prevent score reuse

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import './RuleRow.css'
+import './RuleRow.css';
 
 class RuleRow extends Component {
   render() {
+    const { name, score } = this.props;
+    let disabled = score === undefined ? 'active' : 'disabled';
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr
+        className={`RuleRow RuleRow-${disabled}`}
+        onClick={this.props.doScore}
+      >
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
-    )
+    );
   }
 }
 
